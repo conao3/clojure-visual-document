91350311ba0c1ca6b1aed6372da3afd3de0b2a8c
conao3 <conao3@gmail.com>
2025-01-15 00:56:23 +0900

    fetch statis related files

--- Makefile
+++ Makefile
@@ -10,10 +10,40 @@ format:
 
 .PHONY: build
 build: lock
-	nix build
+	# nix build
+	clojure -X visual-document.core/export
 
 .PHONY: lock
 lock: deps-lock.json
 
 deps-lock.json: deps.edn flake.nix
 	nix run github:jlesquembre/clj-nix#deps-lock -- --deps-include $<
+
+.PHONY: dev
+dev:
+	clojure -X visual-document.core/start-server
+
+.PHONY: deploy
+deploy:
+	npx wrangler pages deploy
+
+generated/spectrum resources/spectrum:
+	mkdir -p $@
+
+resources/spectrum/%.json: resources/spectrum
+	curl -L https://raw.githubusercontent.com/adobe/spectrum-tokens/refs/heads/main/packages/tokens/src/$*.json > $@
+
+generated/spectrum/%.css: generated/spectrum
+	curl -L https://raw.githubusercontent.com/conao3/blog-stasis-src/refs/heads/master/$@ > $@
+
+SPECTRUM_FILE += color-aliases.json
+SPECTRUM_FILE += color-component.json
+SPECTRUM_FILE += color-palette.json
+SPECTRUM_FILE += icons.json
+SPECTRUM_FILE += layout-component.json
+SPECTRUM_FILE += layout.json
+SPECTRUM_FILE += semantic-color-palette.json
+SPECTRUM_FILE += typography.json
+
+.PHONY: fetch
+fetch: $(SPECTRUM_FILE:%=resources/spectrum/%) $(SPECTRUM_FILE:%.json=generated/spectrum/%.css)
--- src/visual_document/core.clj
+++ src/visual_document/core.clj
@@ -1,7 +1,43 @@
 (ns visual-document.core
   (:require
-   [clojure.tools.logging :as log])
+   [clojure.tools.logging :as log]
+   [hiccup.page :as hiccup.page]
+   [hiccup2.core :as hiccup]
+   [stasis.core :as stasis]
+   [ring.adapter.jetty :as jetty])
   (:gen-class))
+
+(def blog-name "clojure.core cheatsheet")
+(def stasis-config {:stasis/ignore-nil-pages? true})
+
+(defn render-page [{:keys [title body]}]
+  (hiccup.page/html5 {:lang "ja"}
+   [:head
+    [:meta {:http-equiv "Content-Type" :content "text/html; charset=utf-8"}]
+    [:meta {:name "viewport" :content "width=device-width,initial-scale=1"}]
+    [:title (format "%s - %s" title blog-name)]
+    (hiccup.page/include-css "/assets/modern-css-reset.css")
+    (hiccup.page/include-css "/assets/spectrum/color-palette.css")
+    (hiccup.page/include-css "/assets/spectrum/typography.css")
+    (hiccup.page/include-css "/assets/index.css")]
+   [:body
+    [:h2 blog-name]
+    [:div {:style {:display "flex"}}
+     [:a {:href "/"} "Home"]]
+    (hiccup/raw body)]))
+
+(defn site []
+  (stasis/merge-page-sources
+   {:public (stasis/slurp-directory "resources/public" #"\.[^.]+$")
+    :spectrum (-> (stasis/slurp-directory "generated/spectrum" #"\.[^.]+$")
+                  (update-keys (partial str "/assets/spectrum")))
+    :static {"/index.html" (render-page {:title "asdf"
+                                         :body "body"})}}))
+
+(defn start-server [& _args]
+  (jetty/run-jetty
+    (stasis/serve-pages site stasis-config)
+    {:port 8080 :join? false}))
 
 (defn -main
   "The entrypoint."
