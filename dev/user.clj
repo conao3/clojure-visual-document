2920e0c3b43aa7d7ca2f80321c035138802d5b32
conao3 <conao3@gmail.com>
2025-01-15 00:27:58 +0900

    define clojure-core-meta

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,7 @@
 (ns user
   (:require
    [clojure.data.json :as json]
+   [clojure.java.io :as io]
    [babashka.fs :as fs]))
 
 (alter-var-root #'*warn-on-reflection* (constantly true))
@@ -19,18 +20,26 @@
   filepath)
 
 (comment
-  (->
-   (->> (ns-publics 'clojure.core)
-        (sort-by (comp name key))
-        (map (comp meta val)))
-   (json/write-str
-    :indent 2
-    :value-fn (fn [_ val]
-                (let [c (class val)]
-                  (cond
-                    (= c clojure.lang.Namespace) (str val)
-                    (= c Class) (str val)
-                    (= c clojure.lang.Var) (str val)
-                    (and (fn? val) (nil? (trap (name val)))) "<lambda>"
-                    :else val))))
-   (->> (spit (ensure-dirs "src/json/clojure.core.json")))))
+  (def clojure-core-meta-raw
+    (->> (ns-publics 'clojure.core)
+         (sort-by (comp name key))
+         (map (comp meta val))))
+
+  (-> clojure-core-meta-raw
+      (json/write-str
+       :indent 2
+       :value-fn (fn [_ val]
+                   (let [c (class val)]
+                     (cond
+                       (= c clojure.lang.Namespace) (str val)
+                       (= c Class) (str val)
+                       (= c clojure.lang.Var) (str val)
+                       (and (fn? val) (nil? (trap (name val)))) "<lambda>"
+                       :else val))))
+      (->> (spit (ensure-dirs "src/json/clojure.core.json"))))
+
+  (def clojure-core-meta
+    (-> "src/json/clojure.core.json"
+        fs/file
+        io/reader
+        json/read-json)))
