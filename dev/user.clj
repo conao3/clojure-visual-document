158bbd57244154b9efd398ffccddaeae8281f0a0
conao3 <conao3@gmail.com>
2025-01-15 00:20:01 +0900

    create clojure.core.json

--- dev/user.clj
+++ dev/user.clj
@@ -1,3 +1,36 @@
-(ns user)
+(ns user
+  (:require
+   [clojure.data.json :as json]
+   [babashka.fs :as fs]))
 
 (alter-var-root #'*warn-on-reflection* (constantly true))
+
+(defmacro trap [& body]
+  `(try
+     (do
+       ~@body)
+     (catch Exception _# nil)))
+
+(defn ensure-dirs [filepath]
+  (-> filepath
+      fs/file
+      fs/parent
+      fs/create-dirs)
+  filepath)
+
+(comment
+  (->
+   (->> (ns-publics 'clojure.core)
+        (sort-by (comp name key))
+        (map (comp meta val)))
+   (json/write-str
+    :indent 2
+    :value-fn (fn [_ val]
+                (let [c (class val)]
+                  (cond
+                    (= c clojure.lang.Namespace) (str val)
+                    (= c Class) (str val)
+                    (= c clojure.lang.Var) (str val)
+                    (and (fn? val) (nil? (trap (name val)))) "<lambda>"
+                    :else val))))
+   (->> (spit (ensure-dirs "src/json/clojure.core.json")))))
