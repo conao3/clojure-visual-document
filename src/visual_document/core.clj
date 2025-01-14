876e7dd5b34112742afa781a1afdac8de408ab39
conao3 <conao3@gmail.com>
2025-01-14 23:24:54 +0900

    generate project

---
+++ src/visual_document/core.clj
@@ -0,0 +1,9 @@
+(ns visual-document.core
+  (:require
+   [clojure.tools.logging :as log])
+  (:gen-class))
+
+(defn -main
+  "The entrypoint."
+  [& args]
+  (log/info args))
---
+++ test/visual_document/core_test.clj
@@ -0,0 +1,6 @@
+(ns visual-document.core-test
+  (:require
+   [clojure.test :as t]))
+
+(t/deftest sample-test
+  (t/is (= true true)))
