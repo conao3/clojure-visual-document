c56becc3440514fbe5400d86022656038750a77d
conao3 <conao3@gmail.com>
2025-01-25 11:18:36 +0900

    update

--- src/visual_document/pages/index.clj
+++ src/visual_document/pages/index.clj
@@ -1,11 +1,5 @@
-(ns visual-document.pages.index
-  (:require
-   [visual-document.resources.core :as c.resources.core]
-   [hiccup2.core :as h]))
+(ns visual-document.pages.index)
 
 (defn page []
   {:title "index"
-   :body (h/html
-          [:ul
-           (map (fn [x] [:li (:name x)])
-                c.resources.core/clojure-core-meta)])})
+   :body nil})
