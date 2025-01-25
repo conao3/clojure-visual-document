e313b2a0387613c15a8764e5c5b60f288bfe45d4
conao3 <conao3@gmail.com>
2025-01-25 10:38:47 +0900

    generate ns files

--- dev/user.clj
+++ dev/user.clj
@@ -1,8 +1,9 @@
 (ns user
   (:require
-   [clojure.data.json :as json]
-   [clojure.java.io :as io]
-   [babashka.fs :as fs]))
+   [babashka.fs :as fs]
+   [clj-yaml.core :as yaml]
+   [clojure.string :as str]
+   [clojure.walk :as walk]))
 
 (alter-var-root #'*warn-on-reflection* (constantly true))
 
@@ -12,34 +13,36 @@
        ~@body)
      (catch Exception _# nil)))
 
-(defn ensure-dirs [filepath]
-  (-> filepath
-      fs/file
-      fs/parent
-      fs/create-dirs)
-  filepath)
+(defn get-ns-map [ns]
+  (->> (ns-publics ns)
+       (map (juxt val (comp meta val)))
+       (into {})))
+
+(defn serialize [val]
+  (->> val
+       (walk/postwalk (fn [val]
+                        (let [c (class val)]
+                          (cond
+                            (= c clojure.lang.Namespace) (str val)
+                            (= c Class) (str val)
+                            (= c clojure.lang.Var) (str val)
+                            (symbol? val) (name val)
+                            (and (fn? val) (nil? (trap (name val)))) "<lambda>"
+                            :else val))))))
+
+(defn generate-ns-files [ns]
+  (-> (get-ns-map ns)
+      (update-vals (comp yaml/generate-string serialize))
+      (->> (map (fn [[key val]]
+                  (let [file (fs/file "resources" "gen" "ns"
+                                      (-> key symbol namespace (str/replace "." "/"))
+                                      (str
+                                       (-> key symbol name
+                                           (str/replace "/" "[s]")
+                                           (str/replace #"[A-Z]" #(str "_" (str/lower-case %1))))
+                                       ".yml"))]
+                    (-> file fs/parent fs/create-dirs)
+                    (->> val (spit file))))))))
 
 (comment
-  (def clojure-core-meta-raw
-    (->> (ns-publics 'clojure.core)
-         (sort-by (comp name key))
-         (map (comp meta val))))
-
-  (-> clojure-core-meta-raw
-      (json/write-str
-       :indent 2
-       :value-fn (fn [_ val]
-                   (let [c (class val)]
-                     (cond
-                       (= c clojure.lang.Namespace) (str val)
-                       (= c Class) (str val)
-                       (= c clojure.lang.Var) (str val)
-                       (and (fn? val) (nil? (trap (name val)))) "<lambda>"
-                       :else val))))
-      (->> (spit (ensure-dirs "src/json/clojure.core.json"))))
-
-  (def clojure-core-meta
-    (-> "src/json/clojure.core.json"
-        fs/file
-        io/reader
-        json/read-json)))
+  (-> (generate-ns-files 'clojure.core) dorun))
